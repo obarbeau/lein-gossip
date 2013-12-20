6e252bcf8e2e969a7c9647501f67ce7ab29182eb
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2013-12-20 12:11:59 -0500

    Reformatted code to idiomatic clojure, and changed version.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-gossip "0.1.0-SNAPSHOT"
+(defproject cc.artifice/lein-gossip "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/artifice-cc/lein-gossip"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
--- src/leiningen/gossip.clj
+++ src/leiningen/gossip.clj
@@ -2,38 +2,43 @@
   (:require [clojure.java.io :as io])
   (:use [leiningen.gossip.core :only [gossip-about]]))
 
-(defn get-parameter-with-cascading-defaults [project args kvec i default]
+(defn get-parameter-with-cascading-defaults
   "Given the project data and the args passed into the task,
   look to see if the args contains something at the given index,
   if it does, return it. If it doesn't, look in the project for the
   key vector [:key1 ... :key2]. If that exists, return it,
   and if not return the default."
+  [project args kvec i default]
   (if (contains? args i)
     (args i)
     (get-in project kvec default)))
 
-(defn extract-source-dir [project args]
+(defn extract-source-dir
   "Extract the source directory passed on the command line or the 
   source-paths in the project.clj file or the default (\"src\")."
+  [project args]
   (get-parameter-with-cascading-defaults project args [:source-paths] 0 "src"))
 
-(defn extract-target-dir [project args]
+(defn extract-target-dir
   "Extract the target directory passed on the command line or the
   value in [:gossip :target] in the project.clj or the default (\"doc/dot\")"
+  [project args]
   (get-parameter-with-cascading-defaults project args [:gossip :target] 1 "doc/dot"))
 
-(defn confirm-dir-exists [dirname]
+(defn confirm-dir-exists
   "Return true if the dirname exists and is a directory."
+  [dirname]
   (let [dir (io/file dirname)]
     (and (.exists dir) (.isDirectory dir))))
 
-(defn create-dir! [dirname]
+(defn create-dir!
   "If the dirname does not exist, create it."
+  [dirname]
   (let [dir (io/file dirname)]
     (if (not (and (.exists dir) (.isDirectory dir)))
       (.mkdirs dir))))
 
-(defn gossip [project & args]
+(defn gossip
   "The main event...
 
   Gossip will attempt to contruct a primitive call graph of the Clojure code
@@ -43,12 +48,12 @@
   It will write the *.dot files to the doc/dot directory and make it if it does not
   exist or it will use [:gossip :target] in project.clj or the supplied value on the
   command line (second parameter)."
-  (let [
-   args-vec (vec args) ;; args is a list and not positional
-   raw-args (extract-source-dir project args-vec)
-   srcs (if (seq? raw-args) (vec raw-args) [raw-args])
-  _ (println (str "Looking for Clojure files in " srcs))
-   tar (extract-target-dir project args-vec)]
+  [project & args]
+  (let [args-vec (vec args) ;; args is a list and not positional
+        raw-args (extract-source-dir project args-vec)
+        srcs (if (seq? raw-args) (vec raw-args) [raw-args])
+        _ (println (str "Looking for Clojure files in " srcs))
+        tar (extract-target-dir project args-vec)]
     (create-dir! tar)
     (doseq [src srcs]
       (if (confirm-dir-exists src)
