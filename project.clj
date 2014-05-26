55b02f765415fbdc858173343055bc43f4c15d1e
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2014-05-26 11:00:16 -0400

    Updated version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.artifice/lein-gossip "0.2.0-SNAPSHOT"
+(defproject cc.artifice/lein-gossip "0.2.1"
   :description "FIXME: write description"
   :url "https://github.com/artifice-cc/lein-gossip"
   :license {:name "Eclipse Public License"
